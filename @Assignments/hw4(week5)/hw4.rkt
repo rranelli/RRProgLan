6574625e344633b6a001aa01a97acdff665970b4
renan-ranelli <renanranelli@gmail.com>
2013-11-10 17:12:17 -0200

    doing the easy problems

--- @Assignments/hw4(week5)/hw4.rkt
+++ @Assignments/hw4(week5)/hw4.rkt
@@ -1,7 +1,28 @@
-
 #lang racket
-
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
 ;; put your code below
 
+;; Problem 1
+(define (sequence low high stride)
+  (if (> low high)
+      null
+      (cons low (sequence (+ low stride) high stride))
+  )
+)
+
+;; Problem 2
+(define (string-append-map xs suffix)
+  (map (lambda(i) (string-append i suffix)) xs)
+  )
+
+;; Problem 3
+(define (list-nth-mod xs n)
+  (if (< n 0)
+      (error "list-nth-mod: negative number")
+      (if (empty? xs)
+          (error "list-nth-mod: empty list")
+          (car (list-tail xs (remainder (length xs) n)))
+          )
+      )
+)
